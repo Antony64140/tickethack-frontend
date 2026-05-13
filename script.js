c4ee3b4e46a9aefe224355ae04b89bbcaecad061
Antony64140 <beaurepere64@gmail.com>
2026-05-13 21:32:24 +0200

    frontend script

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('search-btn').addEventListener('click',
+    function(){
+       let depart = document.querySelector('#departure').value
+       let arriver = document.querySelector('#arrival').value
+       let date = document.querySelector('#date').value
+    }
+)
