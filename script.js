26c792e2263c1afef2855b47bf1b589997dbc416
Antony64140 <beaurepere64@gmail.com>
2026-05-14 08:32:37 +0200

    lien avec le backend

--- script.js
+++ script.js
@@ -1,7 +1,108 @@
-document.querySelector('search-btn').addEventListener('click',
-    function(){
-       let depart = document.querySelector('#departure').value
-       let arriver = document.querySelector('#arrival').value
-       let date = document.querySelector('#date').value
+const searchBtn = document.querySelector('#search-btn');
+
+console.log('JS CONNECTE');
+
+searchBtn.addEventListener('click', async () => {
+
+  const departure = document.querySelector('#departure').value.trim();
+
+  const arrival = document.querySelector('#arrival').value.trim();
+
+  const date = document.querySelector('#date').value;
+
+  const resultsContainer = document.querySelector('#results-container');
+
+
+  // CHAMPS VIDES
+  if (!departure || !arrival || !date) {
+
+    resultsContainer.innerHTML = `
+
+      <img src="./images/notfound.png" alt="not found">
+
+      <div class="line"></div>
+
+      <p>No trip found.</p>
+
+    `;
+
+    return;
+  }
+
+  try {
+
+    const response = await fetch(
+
+      `http://localhost:3000/trips?depart=${departure}&arrivee=${arrival}&jour=${date}`
+
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+  
+    // AUCUN RESULTAT
+    if (!data.result || data.trips.length === 0) {
+
+      resultsContainer.innerHTML = `
+
+        <img src="./images/notfound.png" alt="not found">
+
+        <div class="line"></div>
+
+        <p>No trip found.</p>
+
+      `;
+
+      return;
     }
-)
+
+  
+    // RESET
+    resultsContainer.innerHTML = '';
+
+  
+    // AFFICHAGE TRAJETS
+    data.trips.forEach(trip => {
+
+      resultsContainer.innerHTML += `
+
+        <div class="trip-row">
+
+          <p>
+            ${trip.departure} > ${trip.arrival}
+          </p>
+
+          <p>
+            ${trip.date.$date.slice(11,16)}
+          </p>
+
+          <p>
+            ${trip.price}€
+          </p>
+
+          <button class="book-btn">
+            Book
+          </button>
+
+        </div>
+
+      `;
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    resultsContainer.innerHTML = `
+
+      <img src="./images/notfound.png" alt="not found">
+
+      <div class="line"></div>
+
+      <p>No trip found.</p>
+
+    `;
+  }
+
+});
